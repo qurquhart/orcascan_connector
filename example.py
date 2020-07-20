65f7273a03dbc321888d33ecf17e8d94f46598bd
Qurquhart <quintonurquhart@gmail.com>
2020-07-20 14:54:18 -0700

    example execution of orca_connector()

---
+++ example.py
@@ -0,0 +1,19 @@
+from orca_pull import orca_connector
+
+
+# orcascan integration URL
+integration_url = 'https://api.orcascan.com/sheets/xxxxxxxxxxxxxx'
+
+# netsuite csv data containing inventory items - Required: Internal ID, Master SKU
+netsuite_itemcsv_filepath = 'items.csv'
+
+# postman collection and environment variables
+postman_collection = '.\\postman\\postman_collection.json'
+postman_environment = '.\\postman\\postman_environment.json'
+
+# REQUIRES NEWMAN - https://github.com/postmanlabs/newman
+orca_connector(integration_url,
+               netsuite_itemcsv_filepath,
+               postman_collection,
+               postman_environment
+               )
