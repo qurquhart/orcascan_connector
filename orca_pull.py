5129757a744e6cc9fc8a40a45d3d12bea37c91da
Qurquhart <quintonurquhart@gmail.com>
2020-07-28 10:58:07 -0700

    added logging and .find() to xml

--- orca_pull.py
+++ orca_pull.py
@@ -2,22 +2,39 @@ import xml.etree.ElementTree as ET
 import urllib.request
 import csv
 import os
+from logger import Logger
 
 def orca_connector(integration_url, netsuite_itemcsv_filepath, postman_collection, postman_environment):
     '''extract data from orcascan and compare it to NetSuite item CSV
     output data to json for PATCH request'''
-    
-    # pull xml data
-    orca_url = urllib.request.urlopen(integration_url + '.xml')
 
-    # initialize elementree
-    root = ET.parse(orca_url).getroot()
+    # logging
+    log = Logger("activity_log.txt", "orca_connector")
+    error = Logger("error_log.txt", "orca_connector")
 
-    # for defining range() of loop
-    postcount = len(root)
+    try:
+        # pull xml data
+        orca_url = urllib.request.urlopen(integration_url + '.xml')
 
-    # netsuite csv data containing inventory items - Required: Internal ID, Master SKU
-    input_file = csv.DictReader(open(netsuite_itemcsv_filepath, encoding='utf-8'))
+        # initialize elementree
+        root = ET.parse(orca_url).getroot()
+
+        # for defining range() of loop
+        postcount = len(root)
+
+        log.text('Parsed orcascan URL successfully.')
+
+    except Exception as ex:
+        error.text(f"Cannot parse orcascan URL: {ex}")
+
+    try:
+        # netsuite csv data containing inventory items - Required: Internal ID, Master SKU
+        input_file = csv.DictReader(open(netsuite_itemcsv_filepath, encoding='utf-8'))
+        log.text('NetSuite item data loaded successfully.')
+
+    except Exception as ex:
+        error.text(f"Cannot load NetSuite item data: {ex}")
+
 
     # to be written to json
     output_list = []
@@ -26,8 +43,8 @@ def orca_connector(integration_url, netsuite_itemcsv_filepath, postman_collectio
         for i in range(postcount):
 
             # tree definitions
-            quantity = root[i][6].text
-            master_sku = root[i][11].text
+            quantity = root[i].find("Quantity").text
+            master_sku = root[i].find("Sku").text
 
             if master_sku:
 
@@ -46,15 +63,29 @@ def orca_connector(integration_url, netsuite_itemcsv_filepath, postman_collectio
 
 
     # write CSV file, creating directory if not present
-    os.makedirs(os.path.dirname('output/'), exist_ok=True)
 
-    keys = output_list[0].keys()
+    try:
+        os.makedirs(os.path.dirname('output/'), exist_ok=True)
 
-    with open('output/orca_info.csv', 'w', newline='') as output:
-        dict_writer = csv.DictWriter(output, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(output_list)
+    except Exception as ex:
+        error.text(f'Unable to create "output/" directory.')
 
-    print("File created.")
+    try:
+        keys = output_list[0].keys()
 
-    os.system(f"newman run {postman_collection} -d .\\output\\orca_info.csv -e {postman_environment}")
+        with open('output/orca_info.csv', 'w', newline='') as output:
+            dict_writer = csv.DictWriter(output, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(output_list)
+
+    except Exception as ex:
+        error.text(f'Unable to write to CSV: {ex}')
+
+ 
+    log.text("Created JSON PATCH data.")
+
+    try:
+        os.system(f"newman run {postman_collection} -d .\\output\\orca_info.csv -e {postman_environment}")
+        log.text(f"Successfully PATCHed {len(output_list)} entries to NetSuite.")
+    except Exception as ex:
+        error.text(f'Newman could not run, unable to PATCH NetSuite: {ex}')
